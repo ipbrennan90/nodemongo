a6c94f3db5c1122f2244f2eba2319e34efed7260
ipbrennan <ian.brennan@icloud.com>
2017-01-08 16:09:47 -0700

    fixing dist issue

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,16 @@
+'use strict';
 
 //Express Server
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 // getting server to listen for connections
-app.listen(3000, function() {
+app.listen(3000, function () {
 	console.log('listening on 3000');
 });
 
 // handling a get a request (a READ request to our server)
 app.get('/', function (req, res) {
 	//__dirname is directory that contains JS source code.
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/index.html');
 });
