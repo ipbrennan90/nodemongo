43e7cb30e8ff4723f8d76ed12254d897b467135d
ipbrennan <ian.brennan@icloud.com>
2017-01-09 10:03:13 -0700

    configuring for dev and prod envs

--- server.js
+++
@@ -1,9 +0,0 @@
-
-//Express Server
-const express = require('express');
-const app = express();
-
-// getting server to listen for connections
-app.listen(3000, function() {
-	console.log('listening on 3000');
-});
