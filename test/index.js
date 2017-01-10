e9d787cbc5c78a26cb76bf4b041a9a1158ff7b73
ipbrennan <ian.brennan@icloud.com>
2017-01-10 13:57:19 -0700

    added chai and mocha, started revamping api tests

--- test/index.js
+++ test/index.js
@@ -1,32 +1,39 @@
 import http from 'http';
 import assert from 'assert';
+import chai from 'chai';
+import mocha from 'mocha';
+import chaiHttp from 'chai-http';
+const should = chai.should()
 
-import '../lib/index.js';
-const serverIP = '192.168.0.102'
+import {server} from '../lib/index'
+const serverIP = '192.168.105.199'
 const serverPort = '3000'
-const serverAddress = `http://${serverIP}:${serverPort}`
-describe('Exampe Node Server', () => {
-	it('should return 200', done => {
-		http.get(serverAddress, res => {
-			assert.equal(200, res.statusCode);
+const serverAddress = `http://localhost:${serverPort}/temps`
+chai.use(chaiHttp);
+describe('Get Temps', () => {
+	it('should return get all temps', done => {
+		chai.request(server)
+			.get('/temps')
+			.end((err, res) => {
+				res.should.have.status(200);
 			done();
 		});
 	});
 });
 
-describe('Post request', () => {
-
-	it('should return 200', done => {
-		const post_options = {
-			host: serverIP,
-			port: serverPort,
-			path: '/temps',
-			method: 'POST',
-			data: {}
-		};
-		http.request(post_options, res => {
-			assert.equal(200, res.statusCode);
-			done();
-		});
-	});
-});
+// describe('Post request', () => {
+//
+// 	it('should return 200', done => {
+// 		const post_options = {
+// 			host: 'localhost',
+// 			port: serverPort,
+// 			path: '/temps',
+// 			method: 'POST',
+// 			data: {}
+// 		};
+// 		http.request(post_options, res => {
+// 			assert.equal(200, res.statusCode);
+// 			done();
+// 		});
+// 	});
+// });
